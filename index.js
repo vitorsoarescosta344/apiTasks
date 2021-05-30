61f06d53746f14b2c0338a0e6a0547f81e88f330
victor <70414722+vitorsoarescosta344@users.noreply.github.com>
2021-05-30 19:44:12 -0300

    Versão final

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const db = require('./config/db')
+const consign = require('consign')
+const PORT = process.env.PORT || 5000
+
+
+consign()
+    .include('./config/passport.js')
+    .then('./config/middlewares.js')
+    .then('./api')
+    .then('./config/routes.js')
+    .into(app)
+
+app.db = db
+
+app.listen(PORT, () => {
+    console.log('Backend executando...')
+})
